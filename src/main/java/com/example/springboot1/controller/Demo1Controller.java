638ad4aabc951197fa92e84c29afcda4710d8d6d
wangwc <1045366416@qq.com>
2019-04-24 11:24:41 +0800

    commit2

---
+++ src/main/java/com/example/springboot1/controller/Demo1Controller.java
@@ -0,0 +1,20 @@
+package com.example.springboot1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value="/Portal")
+public class Demo1Controller {
+    @RequestMapping(value="/test001")
+    @ResponseBody
+    public Map gitHubTest(String name){
+        Map map = new HashMap();
+        map.put("name",name);
+        return map;
+    }
+}
